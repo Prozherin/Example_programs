c19fdde596fddd6be996904af0fb3682d4b4fa0b
Prozherin <taskacik@gmail.com>
2023-08-03 20:52:21 +0500

    add Example 011 Array Library (Внесли дополнительные функции генерации).

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -9,7 +9,6 @@
     }
 }
 
-
 void PrintArray(int[] col)
 {
     int count = col.Length;
@@ -21,7 +20,32 @@ void PrintArray(int[] col)
     }
     return;
 }
+
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position =0;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
 PrintArray(array);
+Console.WriteLine();
+
+int pos = Index0f(array, 4);
+Console.WriteLine(pos);
